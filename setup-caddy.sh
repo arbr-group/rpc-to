78d524bde3a2db7f184f1ce25387564f7e5066a7
Ari V <11803028+arivictor@users.noreply.github.com>
2022-05-01 03:03:03 +1000

    Update setup-caddy.sh

--- setup-caddy.sh
+++ setup-caddy.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
 
--set e
+set -e
 
+# Install Caddy
 apt-get update -qqy && apt-get install -qqy debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | tee /etc/apt/trusted.gpg.d/caddy-stable.asc
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 apt-get update -qqy && apt-get install -qqy caddy
+
+# Start Caddyfile
+sudo cp Caddyfile /etc/caddy/Caddyfile
+caddy adapt --config /etc/caddy/Caddyfile
+systemctl restart caddy
