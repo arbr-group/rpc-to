20354629310668fd4990f6dcb201b8b3f9e002b2
Ari V <11803028+arivictor@users.noreply.github.com>
2022-05-01 02:59:55 +1000

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+export VERSION=1.10.11
+export SOLANA_HOME=/sol
+export SOLANA_USER=sol
+export RUN_SCRIPT=${SOLANA_HOME}/validator.sh
+export SERVICE=/etc/systemd/system/sol.service
