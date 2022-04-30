cc16a733271d9d5fe87955e6cf09c47665dab642
Ari V <11803028+arivictor@users.noreply.github.com>
2022-05-01 03:08:23 +1000

    Update setup-user.sh

--- setup-user.sh
+++ setup-user.sh
@@ -4,3 +4,7 @@ set -e
 
 # Create RPC User
 adduser --disabled-password --gecos "" --home "${SOLANA_HOME}" "${SOLANA_USER}"
+
+# Give User Access to Dir
+sudo chown -R ${SOLANA_USER}:${SOLANA_USER} ${SOLANA_HOME}
+sudo -iu ${SOLANA_USER} mkdir -p ${SOLANA_HOME}/ledger
