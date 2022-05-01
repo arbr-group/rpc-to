c84659a4fd4fcc262c7b14c1b4da64679a2b1619
Ari V <11803028+arivictor@users.noreply.github.com>
2022-05-01 14:23:12 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,33 +10,11 @@ chmod +x setup-validator.sh
 chmod +x setup-service.sh
 chmod +x setup-validator-keys.sh
 
-if [[ ! -e /etc/caddy/Caddyfile ]]; then
-  echo "Setting up Caddy..."
-  sh setup-caddy.sh
-fi
-
-if [[ ! -e $SOLANA ]]; then
-  echo "Setting up Solana CLI..."
-  sh setup-solana.sh
-fi
-
-if [[ -d $SOLANA_HOME ]]; then
-  echo "Setting up User..."
-  sh setup-user.sh
-fi
-
-if [[ ! -e $RUN_SCRIPT ]]; then
-  echo "Setting up Validator..."
-  sh setup-validator.sh
-fi
-
-echo "Setting up Systemctl Service..."
+sh setup-caddy.sh
+sh setup-solana.sh
+sh setup-user.sh
+sh setup-validator.sh
 sh setup-service.sh
+sh setup-validator-keys.sh
 
-if [[ ! -e "${SOLANA_HOME}/validator-key.json" ]]; then
-  echo "Setting up Credentials..."
-  sh setup-validator-keys.sh
-fi
-
-echo "Starting Service..."
 systemctl enable --now sol
