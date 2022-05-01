3e20cdd7f65b20fbc2a66e7b944b92b6fa8ffa9a
Ari Victor <11803028+arivictor@users.noreply.github.com>
2022-05-01 17:56:40 +1000

    u

--- startup.sh
+++ startup.sh
@@ -11,10 +11,10 @@ RUN_SCRIPT=${SOLANA_HOME}/validator.sh
 SERVICE_NAME=wonka
 SERVICE_FILE=/etc/systemd/system/${SERVICE_NAME}.service
 
+apt get update
 
-sleep 60 # wait for apt, sorry mom i know it's ghetto
 # install caddy
-apt-get update -qqy && apt-get install -qqy debian-keyring debian-archive-keyring apt-transport-https
+apt-get install -qqy debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | tee /etc/apt/trusted.gpg.d/caddy-stable.asc
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 apt-get update -qqy && apt-get install -qqy caddy
