0085c3e2e9ef4132d6fceb671f2c4f50d43e9d57
Ari V <11803028+arivictor@users.noreply.github.com>
2022-05-01 15:04:07 +1000

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,6 +3,7 @@
 VERSION=1.10.11
 SOLANA_HOME=/sol
 SOLANA_USER=sol
+ENVIRONMENT=devnet
 RUN_SCRIPT=${SOLANA_HOME}/validator.sh
 SOLANA=/usr/share/solana-release/bin/solana
 SOLANA_VALIDATOR=/usr/share/solana-release/bin/solana-validator
