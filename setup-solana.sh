a8cf04212ddff204458c71536c9ff550c94f75e0
Ari Victor <11803028+arivictor@users.noreply.github.com>
2022-05-01 16:47:59 +1000

    update

--- setup-solana.sh
+++ setup-solana.sh
@@ -8,4 +8,4 @@ set -e
 cd "$(mktemp -d)" || exit
 wget https://github.com/solana-labs/solana/releases/download/v${VERSION}/solana-release-x86_64-unknown-linux-gnu.tar.bz2
 tar jxf solana-release-x86_64-unknown-linux-gnu.tar.bz2
-mv solana-release /usr/share/
+sudo mv solana-release /usr/share/
